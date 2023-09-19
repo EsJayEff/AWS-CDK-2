2cc20dd48eb64d0330852cb04cbf68cdf0c4f09e
Shiraz Javed Farooq <114878673+EsJayEff@users.noreply.github.com>
2023-09-19 22:14:46 +0500

    SNS In Progress

---
+++ step17_simple_notification_service/step00_httpsSubscription/lib/step17_simple_notification_service-stack.ts
@@ -0,0 +1,39 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
+import { SubscriptionProtocol } from "aws-cdk-lib/aws-sns";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+
+
+export class Step17SimpleNotificationServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // create a lambda function
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+
+    });
+
+    // create an endpoint for the lambda function
+    const api = new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
+
+    // create an SNS topic
+    const myTopic = new sns.Topic(this, "MyTopic");
+
+    // The following command subscribes our endpoint(connected to lambda) to the SNS topic
+    myTopic.addSubscription(
+      new subscriptions.UrlSubscription(api.url, {
+        protocol: SubscriptionProtocol.HTTPS
+      })
+    );
+
+
+  }
+}
